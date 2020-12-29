a33af8aee6346abd4d2986aea0e2a48882008f09
vagrant <vagrant@localhost.localdomain>
2020-12-29 15:13:47 +0000

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,12 @@ class Post < ApplicationRecord
   attachment :image
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 
   validates :image, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :text, presence: true, length: { maximum: 200 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'post#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index, :show, :destroy] do
-    resources :comments, only:[:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
 
   resources :users, only: [:show, :edit, :update]
