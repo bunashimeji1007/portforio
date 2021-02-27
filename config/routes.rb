1a53efd88fb9ab76827051398a02b8fe9973db7c
vagrant <vagrant@localhost.localdomain>
2021-02-27 04:12:42 +0000

    favorite and mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'post#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
 
